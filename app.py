e524ff3438f6bab6ca7b6cecb3b8288bb95d7729
Corbett Austin <corbettaustin@Corbetts-MacBook-Air.local>
2020-05-21 18:56:54 -0500

    added templates

--- app.py
+++ app.py
@@ -1,14 +1,27 @@
 import pandas as pd
 import os
 import json
-from flask import Flask, jsonify
+from flask import Flask, jsonify, render_template
 
 
 app = Flask(__name__)
-app.config[""] = (
-
+app.config["DEBUG"] = True
 
 @app.route("/")
 def index():
     result = render_template("index.html")
     return result
+
+@app.route("/")
+def page_two():
+    result = render_template("page_two.html")
+    return result
+
+@app.route("/")
+def page_three():
+    result = render_template("page_three.html")
+    return result
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
